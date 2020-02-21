fe95c30f6a098adfb58997a19d119e2d47b13b58
Noah Mitchell <21.noah.mitchell@ksd.org>
2020-02-21 09:25:42 -0800

    Update

--- fixMobileTactics/Assets/Scripts/BattleSystem.cs
+++ fixMobileTactics/Assets/Scripts/BattleSystem.cs
@@ -27,6 +27,7 @@ public class BattleSystem : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
 		state = BattleState.START;
 		StartCoroutine(SetupBattle());
     }
